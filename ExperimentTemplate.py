cb49f8299d61f1e3876e0e03304d2165b191805b
Niklas Schwind <niklas.schwind@climateanalytics.org>
2022-06-13 11:11:10 +0200

    New Experiments

--- ExperimentTemplate.py
+++ ExperimentTemplate.py
@@ -8,10 +8,10 @@ import ExpectedGradientLength as Expe
 import CoreSet as Core
 
 #flair.set_seed(100)
-filename_results = 'results_CoreSet_seedset_1.txt'
-filename_model = 'resources/taggers/CoreSet1'
-filename_model2 = 'resources/taggers/CoreSet1'
-device = 'cuda:1'
+filename_results = 'results_CoreSet_seedset_2.txt'
+filename_model = 'resources/taggers/CoreSet2'
+filename_model2 = 'resources/taggers/CoreSet2'
+device = 'cuda:2'
 SeedSet = True
 shuffle = True
 Exp = 3  #1,2 oder 3
