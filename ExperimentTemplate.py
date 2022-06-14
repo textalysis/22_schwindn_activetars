eaec97697c0f146fd5959d55961300b34c7f4a94
Niklas Schwind <niklas.schwind@climateanalytics.org>
2022-06-14 09:27:27 +0200

    fixed savepath bug on server

--- ExperimentTemplate.py
+++ ExperimentTemplate.py
@@ -8,13 +8,13 @@ import ExpectedGradientLength as Expe
 import CoreSet as Core
 
 #flair.set_seed(100)
-filename_results = 'results_ExpGrad_noseedset_1.txt'
-filename_model = 'resources/taggers/ExpGrad7'
-filename_model2 = 'resources/taggers/ExpGrad7'
-device = 'cuda:0'
+filename_results = 'results_CoreSet_noseedset_1.txt'
+filename_model = 'resources/taggers/CoreSet1'
+filename_model2 = 'resources/taggers/CoreSet1'
+device = 'cuda:1'
 SeedSet = False
 shuffle = True
-Exp = 2  #1,2 oder 3
+Exp = 3  #1,2 oder 3
 
 def write(name, contents):
     with open(filename_results, 'w', encoding='utf-8') as f:
