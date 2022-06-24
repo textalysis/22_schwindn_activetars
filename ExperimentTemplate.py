10e80cccfaea0c27ec3b10e7e88b74cba31375ce
Niklas Schwind <niklas.schwind@climateanalytics.org>
2022-06-24 18:25:07 +0200

    fixed savepath bug on server

--- ExperimentTemplate.py
+++ ExperimentTemplate.py
@@ -11,14 +11,14 @@ from flair.datasets import ClassificationCorpus
 
 #flair.set_seed(100)
 LabelType = 'topic'
-filename_results = 'results_CoreSet_Stackoverflow_noseedset_3.txt'
-filename_model = 'resources/taggers/CoreSet'
-filename_model2 = 'resources/taggers/CoreSet'
-device = 'cuda:1'
+filename_results = 'results_ExpGrad_Stackoverflow_noseedset_1.txt'
+filename_model = 'resources/taggers/ExpGradNo'
+filename_model2 = 'resources/taggers/ExpGradNo'
+device = 'cuda:0'
 SeedSet = False
 shuffle = True
 
-Exp = 3  #1,2 oder 3
+Exp = 2  #1,2 oder 3
 
 def write(name, contents):#, alg1, alg2lol, alg2):
     with open(filename_results, 'w', encoding='utf-8') as f:
