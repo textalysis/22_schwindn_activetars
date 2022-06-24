98cc288b4c0fad09a49cc62bf567b0ad95df0bd1
Niklas Schwind <niklas.schwind@climateanalytics.org>
2022-06-24 11:19:21 +0200

    fixed savepath bug on server

--- CreateRandomSeedSet.py
+++ CreateRandomSeedSet.py
@@ -6,7 +6,7 @@ import Random as Rand
 TREC_Random = flair.datasets.STACKOVERFLOW().downsample(0.5)
 #Random = Rand.Random(corpus = TREC_Random, TARS = TARS_Random)
 path = '/vol/fob-vol7/mi19/schwindn/.flair/datasets/stackoverflow'
-with open(path+'/train.txt', 'w') as f:
+with open(path+'/trainlol.txt', 'w') as f:
     for data in TREC_Random.train:
         f.write(f'__label__{data.labels[0].value} {data.to_plain_string()}\n')
 with open(path+'/dev.txt', 'w') as f:
