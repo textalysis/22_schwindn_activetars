ecf8a76341711960df846240c8fc0d8c02ed5d53
Niklas Schwind <niklas.schwind@climateanalytics.org>
2022-09-20 14:22:06 +0200

    bug fixes

--- ExperimentTemplate.py
+++ ExperimentTemplate.py
@@ -10,13 +10,13 @@ from flair.data import Corpus
 from flair.datasets import ClassificationCorpus
 
 #flair.set_seed(100)
-modeCoreSet = 'kCenter'
+modeCoreSet = 'kMeans'
 oppositeDirection = False
 LabelType = 'topic'
-filename_results = 'results_CoreSet_TREC_noseedset_NEW1_Center.txt' #'results_CoreSet_TREC_noseedset_NEW1.txt' is kCenter
-filename_model = 'resources/taggers/CoreSetCenter1'
-filename_model2 = 'resources/taggers/CoreSetCenter1'
-device = 'cuda:0'
+filename_results = 'results_CoreSet_TREC_noseedset_NEW2_Means.txt' #'results_CoreSet_TREC_noseedset_NEW1.txt' is kCenter
+filename_model = 'resources/taggers/CoreSetMeans1'
+filename_model2 = 'resources/taggers/CoreSetMeans1'
+device = 'cuda:2'
 SeedSet = False
 shuffle = True
 
